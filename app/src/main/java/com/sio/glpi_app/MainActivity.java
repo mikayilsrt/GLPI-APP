d0be02a57219f34a87559e5da4b5c5bf871048b0
eleve eleve <eleve@sio.local>
2019-01-24 16:23:00 +0100

    Refactoring

--- app/src/main/java/com/sio/glpi_app/MainActivity.java
+++ app/src/main/java/com/sio/glpi_app/MainActivity.java
@@ -4,18 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText nom;
+    private EditText prenom;
     private Operateur lOperateur;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,10 +39,21 @@ public class MainActivity extends AppCompatActivity {
         saveOperateur.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String nom = findViewById(R.id.txtNom).toString();
-                lOperateur = new Operateur(nom);
+                nom = (EditText)findViewById(R.id.txtNom);
+                prenom = (EditText)findViewById(R.id.txtPrenom);
+                //lOperateur = new Operateur(nom.getText(), prenom.getText());
+                Dialogue();
             }
+
         });
+
+    }
+
+    public void Dialogue(){
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this );
+        builder1.setMessage(nom.getText() + " " + prenom.getText());
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 
     public void openNewActivity () {
--- app/src/main/java/com/sio/glpi_app/Operateur.java
+++ app/src/main/java/com/sio/glpi_app/Operateur.java
@@ -1,13 +1,21 @@
 package com.sio.glpi_app;
 
 public class Operateur {
-    private int user_id;
     private String prenom;
     private String nom;
 
-    public Operateur(int u_id, String p, String n){
-        this.user_id = u_id;
+    public Operateur(String p, String n){ ;
         this.prenom = p;
         this.nom = n;
     }
+
+    public String getNom(){
+        return this.nom;
+    }
+
+    public String getPrenom(){
+        return this.prenom;
+    }
+
+
 }
